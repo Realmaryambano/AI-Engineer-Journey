21ae2e0b3d115b0661b6b93fe431f2ab9b594653
Maryam Bano <maryambano.official@gmail.com>
2026-06-27 23:01:11 +0500

    Update instance_vs_class_attribute.py

--- Day 1/OOP/instance_vs_class_attribute.py
+++ Day 1/OOP/instance_vs_class_attribute.py
@@ -6,3 +6,6 @@ emp1 = Employee()
 emp1.language = "Java"
 
 print(emp1.company, emp1.language)
+
+# Note: Instance attributes take preference over class attributes during assignment & retrieval.
+# Output: Google Java
