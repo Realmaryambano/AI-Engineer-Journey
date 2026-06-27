a511fb45482fa7eeeb0f5757a01ce31f1649f8a6
Maryam Bano <maryambano.official@gmail.com>
2026-06-27 23:16:17 +0500

    Create self_parameter.py

---
+++ Day 1/OOP/self_parameter.py
@@ -0,0 +1,10 @@
+class Student:
+    age = 18
+    school = "Tisj"
+
+    def getInfo(self):
+        print(f'{self.boyName} is a good boy. His school name is {self.school} and his age is {self.age}')
+
+std1 = Student()
+std1.boyName = "Muhammad"
+print(std1.getInfo())
