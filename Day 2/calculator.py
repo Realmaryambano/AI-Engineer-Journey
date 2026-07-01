3e0b3fdfd468d52559bbde9a528d399a5ad9c72d
Maryam Bano <maryambano.official@gmail.com>
2026-07-01 17:44:46 +0500

    Add day 2 and 3 practice files
    Adds simple Python class अभ्यास scripts for `Calculator` and `Employee`, plus a basic Day 3 web page scaffold with HTML, CSS, and a placeholder JavaScript file.

---
+++ Day 2/calculator.py
@@ -0,0 +1,7 @@
+class Calculator:
+    def square(self):
+            print(f'The square is {self.number * self.number}')
+
+num1 = Calculator()
+num1.number = 2
+num1.square()
---
+++ Day 2/self_again.py
@@ -0,0 +1,10 @@
+class Employee:
+
+
+    def get_info(self):
+        print(f'My name is {self.name}')
+
+emp1 = Employee()
+emp1.name = "Maryam"
+emp1.get_info()
+
