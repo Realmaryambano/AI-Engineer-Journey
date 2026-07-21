4b64709925a9a5e6543d33cead89977047995b23
Maryam Bano <maryambano.official@gmail.com>
2026-07-21 12:22:49 +0500

    Update Practice_Task.sql

--- Day 10/Oracle SQL/Practice_Task.sql
+++ Day 10/Oracle SQL/Practice_Task.sql
@@ -1,7 +1,6 @@
 --📝 YOUR PRACTICE TASKS — UNIVERSITY DATABASE
 --
 --Use your existing DEPARTMENT, STUDENTS, and COURSE tables.
---
 --🟢 LEVEL 1 — Basic SELECT
 
 --Q1. Display all records from the DEPARTMENT table.
@@ -14,5 +13,19 @@ SELECT DEPARTMENT_NAME,hod_name FROM department;
 SELECT * FROM STUDENTS;
 
 --Q4. Display only STUDENT_NAME, EMAIL, and PHONE_NUMBER.
---
+SELECT STUDENT_NAME, EMAIL, PHONE_NUMBER FROM STUDENTS;
+
 --Q5. Display all records from the COURSE table.
+SELECT * FROM COURSE;
+
+
+
+
+--🟢 LEVEL 2 — DISTINCT
+--Q6. Display the unique buildings from the DEPARTMENT table.
+SELECT DISTINCT BUILDING FROM DEPARTMENT;
+--Q7. Display the unique admission years from the STUDENTS table.
+SELECT DISTINCT admission_year FROM STUDENTS;
+--Q8. Display the unique semester numbers from the COURSE table.
+SELECT DISTINCT SEMESTER FROM COURSE ORDER BY SEMESTER;
+
