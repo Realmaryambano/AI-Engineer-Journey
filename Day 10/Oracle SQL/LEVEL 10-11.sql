49d6e872c69b740a67d595be0d9d8f513b648f5e
Maryam Bano <maryambano.official@gmail.com>
2026-07-21 16:10:16 +0500

    Update LEVEL 10-11.sql

--- Day 10/Oracle SQL/LEVEL 10-11.sql
+++ Day 10/Oracle SQL/LEVEL 10-11.sql
@@ -1,17 +1,23 @@
-
 --🔴 LEVEL 10 — CONCATENATION
---
 --Q41. Display student names with their email in this format:
---
 --Maryam Bano - maryam@gmail.com
---
+SELECT student_name || ' - ' || email as students_email FROM students;
+
 --Q42. Display department information in this format:
---
 --Computer Science is located in Block A
---
+SELECT Department_name || ' is located in '|| building as information FROM department;
+
 --Q43. Display course information in this format:
---
 --Database Management Systems has 3 credit hours
+SELECT course_name || ' has '|| credit_hours || ' credit hours' as information FROM course;
+
+
+SELECT * FROM department;
+SELECT * FROM students;
+SELECT * FROM course;
+
+
+
 --🔴 LEVEL 11 — OPERATIONS
 --
 --Q44. Display each course's credit hours multiplied by 2.
