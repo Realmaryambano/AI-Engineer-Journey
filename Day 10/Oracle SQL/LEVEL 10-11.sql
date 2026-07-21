e25622c8b02a79e2fbd0dd593a9c0ac15aa37e60
Maryam Bano <maryambano.official@gmail.com>
2026-07-21 15:58:37 +0500

    Create LEVEL 10-11.sql

---
+++ Day 10/Oracle SQL/LEVEL 10-11.sql
@@ -0,0 +1,24 @@
+
+--🔴 LEVEL 10 — CONCATENATION
+--
+--Q41. Display student names with their email in this format:
+--
+--Maryam Bano - maryam@gmail.com
+--
+--Q42. Display department information in this format:
+--
+--Computer Science is located in Block A
+--
+--Q43. Display course information in this format:
+--
+--Database Management Systems has 3 credit hours
+--🔴 LEVEL 11 — OPERATIONS
+--
+--Q44. Display each course's credit hours multiplied by 2.
+--
+--Q45. Display each course's credit hours plus 1.
+--
+--Q46. Display the semester number plus 1 for each course.
+--
+--Q47. Display the admission year plus 4 for each student.
+--
