b9ecb84c80686b477c030d3d40d0ddf012c63e29
Maryam Bano <maryambano.official@gmail.com>
2026-07-21 12:19:34 +0500

    Create Practice_Task.sql

---
+++ Day 10/Oracle SQL/Practice_Task.sql
@@ -0,0 +1,18 @@
+--📝 YOUR PRACTICE TASKS — UNIVERSITY DATABASE
+--
+--Use your existing DEPARTMENT, STUDENTS, and COURSE tables.
+--
+--🟢 LEVEL 1 — Basic SELECT
+
+--Q1. Display all records from the DEPARTMENT table.
+SELECT * FROM department;
+
+--Q2. Display only DEPARTMENT_NAME and HOD_NAME.
+SELECT DEPARTMENT_NAME,hod_name FROM department;
+
+--Q3. Display all records from the STUDENTS table.
+SELECT * FROM STUDENTS;
+
+--Q4. Display only STUDENT_NAME, EMAIL, and PHONE_NUMBER.
+--
+--Q5. Display all records from the COURSE table.
