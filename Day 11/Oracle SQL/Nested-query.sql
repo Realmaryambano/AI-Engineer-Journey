cee4d24d65b826a97796fc6df580b20bcfd7a3fe
Maryam Bano <maryambano.official@gmail.com>
2026-07-22 10:38:29 +0500

    Update Nested-query.sql

--- Day 11/Oracle SQL/Nested-query.sql
+++ Day 11/Oracle SQL/Nested-query.sql
@@ -13,7 +13,13 @@ WHERE DEPARTMENT_ID =
 SELECT * FROM COURSE WHERE DEPT_ID = (SELECT DEPARTMENT_ID FROM STUDENTs WHERE STUDENT_NAME = 'Hassan Raza');
 
 --Q3. Find the department name of the department that offers 'Machine Learning'.
---
+SELECT DEPARTMENT_NAME FROM DEPARTMENT WHERE DEPARTMENT_ID = 
+(SELECT DEPT_ID FROM COURSE WHERE COURSE_NAME = 'Machine Learning');
+
 --Q4. Find all students who belong to the department that offers 'Database Management Systems'.
---
+SELECT STUDENT_NAME FROM STUDENTS WHERE DEPARTMENT_ID = 
+(SELECT DEPT_ID FROM COURSE WHERE COURSE_NAME = 'Database Management Systems' );
+
 --Q5. Find all courses offered by the same department as the department of 'Sara Khan'.
+SELECT * FROM COURSE WHERE DEPT_ID = 
+(SELECT DEPARTMENT_ID FROM STUDENTS WHERE STUDENT_NAME = 'Sara Khan' );
