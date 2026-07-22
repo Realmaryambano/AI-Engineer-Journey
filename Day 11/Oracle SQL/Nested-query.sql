7f8ee9d044f0389384eb4486c52e6d8c78960e20
Maryam Bano <maryambano.official@gmail.com>
2026-07-22 10:28:41 +0500

    Update Nested-query.sql

--- Day 11/Oracle SQL/Nested-query.sql
+++ Day 11/Oracle SQL/Nested-query.sql
@@ -1,3 +1,19 @@
 SELECT * FROM DEPARTMENT;
 SELECT * FROM STUDENTS;
 SELECT * FROM COURSE;
+
+--Nested Query Challenge
+--
+--Q1. Find all students who belong to the same department as Maryam Bano.
+SELECT * FROM STUDENTS
+WHERE DEPARTMENT_ID =
+(SELECT DEPARTMENT_ID FROM Students WHERE Student_name = 'Maryam Bano');
+
+--Q2. Find all courses offered by the same department where Hassan Raza studies.
+SELECT * FROM COURSE WHERE DEPT_ID = (SELECT DEPARTMENT_ID FROM STUDENTs WHERE STUDENT_NAME = 'Hassan Raza');
+
+--Q3. Find the department name of the department that offers 'Machine Learning'.
+--
+--Q4. Find all students who belong to the department that offers 'Database Management Systems'.
+--
+--Q5. Find all courses offered by the same department as the department of 'Sara Khan'.
